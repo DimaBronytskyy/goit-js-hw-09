1f3426f8c60f72f6e58cf06e1b619b3ba53c82c2
DimaBronytskyy <138112501+DimaBronytskyy@users.noreply.github.com>
2023-11-30 09:53:50 +0200

    update color 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,7 +4,7 @@ const refs = {
   body: document.querySelector('body'),
 };
 
-let colorInterval = 0;
+let colorInterval = null;
 
 refs.btnStart.addEventListener('click', startColorChange);
 refs.btnStop.addEventListener('click', stopColorChange);
@@ -12,12 +12,16 @@ refs.btnStop.addEventListener('click', stopColorChange);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
 
 function startColorChange() {
   refs.btnStart.disabled = true;
   refs.btnStop.disabled = false;
+
+  // Очистити попередній інтервал перед створенням нового
+  clearInterval(colorInterval);
+
   colorInterval = setInterval(() => {
     refs.body.style.backgroundColor = getRandomHexColor();
   }, 100);
